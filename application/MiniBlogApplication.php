2c257c50c1b79e33d6aa0c394c91fb8ab6b5749f
Tsukasa Endo <entsuka70@gmail.com>
2020-04-16 08:50:04 +0900

    feat: アプリケーションクラスとフロントコントローラーの書き換え

---
+++ application/MiniBlogApplication.php
@@ -0,0 +1,36 @@
+<?php
+
+    class MiniBlogApplication extends Application {
+
+        protected $login_action = array('account', 'signin');
+
+        public function getRootDir() {
+            
+            return dirname(__FILE__);
+
+        }
+
+        // ルーティング定義配列を返すメソッド
+        protected function registerRoutes() {
+
+            return array(
+
+            );
+
+        }
+
+        // アプリケーション設定を行うメソッド
+        protected function configure() {
+
+            $this->db_manager->connect('master', array(
+                'dsn' => 'mysql:dbname=miniblog_phpframework;host=localhost',
+                'user' => 'root',
+                'password' => '',
+            ));
+
+        }
+
+
+    }
+
+?>
