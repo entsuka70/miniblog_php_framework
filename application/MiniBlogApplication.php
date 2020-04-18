297fea21e64d1d11dfa3fa73de4025d679ada6bb
Tsukasa Endo <entsuka70@gmail.com>
2020-04-18 16:49:27 +0900

    feat: signup Actionの実装

--- application/MiniBlogApplication.php
+++ application/MiniBlogApplication.php
@@ -14,7 +14,10 @@
         protected function registerRoutes() {
 
             return array(
-
+                '/account'
+                    => array('controller' => 'account', 'action' => 'index'),
+                '/account/:action'
+                    => array('controller' => 'account'),
             );
 
         }
