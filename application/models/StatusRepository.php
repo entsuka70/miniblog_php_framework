a7e7277c5432d51f87c04e5c7224830e9f0772bf
Tsukasa Endo <entsuka70@gmail.com>
2020-04-18 21:40:40 +0900

    feat: statusコントローラーとレポジトリクラスの機能追加

--- application/models/StatusRepository.php
+++ application/models/StatusRepository.php
@@ -30,4 +30,31 @@
 
         }
 
+        // ユーザーIDに一致する投稿を投稿びの降順に全権取得
+        public function  fetchAllByUserId($user_id) {
+
+            $sqk = "SELECT a.*, u.user_name
+                    FROM status a LEFT JOIN user u ON a.user_id = u.id
+                    WHERE u.id = :user_id
+                    ORDER BY a.created_at DESC
+                    ";
+
+            return $this->fetchAll($sql, array(':user_id' => $user_id));
+        }
+
+        // 投稿IDとユーザーのidに一致するレコードを一件取得
+        public function fetchByIdAndUserName($id, $user_name) {
+
+            $sql = "SELECT a.*, u.user_name
+                    FROM status a LEFT JOIN user u ON u.id = a.user_id
+                    WHERE a.id = :id
+                    AND u.user_name = :user_name
+                    ";
+
+            return $this->fetch($sql, array(
+                ';id' => $id,
+                ':user_name' => $user_name,
+            ));
+        }
+
     }
